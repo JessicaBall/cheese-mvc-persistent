53e183cb26f4c82bc77e02784d0ff06bbdf6dcbb
Jessica Ball <jessica.maree.ball@gmail.com>
2017-04-25 18:06:52 -0500

    Replaced CheeseType enums with Cheese Categories

--- src/main/java/org/launchcode/models/Category.java
+++ src/main/java/org/launchcode/models/Category.java
@@ -1,10 +1,10 @@
 package org.launchcode.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by jessi on 4/6/2017.
@@ -24,6 +24,10 @@ public class Category {
     public Category(String name) {
         this.name = name;
     }
+
+    @OneToMany
+    @JoinColumn(name = "category_id")
+    private List<Cheese> cheeses = new ArrayList<>();
 
     public Category () {}
 
